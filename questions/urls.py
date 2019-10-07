2b0d44c87982b4fd5bb1e2cc9410751a998a6031
Seon Yang <ergaster91@gmail.com>
2019-10-07 16:58:53 +0900

    20191007 django

--- questions/urls.py
+++ questions/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('<int:id>/', views.detail, name='detail'),
 
     path('create/', views.create, name='create'),
+
+    path('<int:question_id>/answers/', views.answer_create, name='answer_create'),
+    path('<int:question_id>/answers/<int:answer_id>/delete/',
+         views.answer_delete, name='answer_delete')
 ]
